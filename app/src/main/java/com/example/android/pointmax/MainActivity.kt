8e13f9e8ad006e06beb96dc9ead2e232e8cf45bd
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-03-27 20:46:07 -0400

    Added log messages to test if it shows in the new branch.

--- app/src/main/java/com/example/android/pointmax/MainActivity.kt
+++ app/src/main/java/com/example/android/pointmax/MainActivity.kt
@@ -1,30 +1,46 @@
 package com.example.android.pointmax
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.android.pointmax.databinding.FragmentHomeBinding
+import com.example.android.pointmax.ui.home.HomeFragment
+import com.example.android.pointmax.ui.recommended.RecommendedFragment
+import com.example.android.pointmax.ui.wallet.WalletFragment
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         // Plant tree to enable Debugging with Timber
         Timber.plant(Timber.DebugTree())
 
-        setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_wallet, R.id.navigation_recommended))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
