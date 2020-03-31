63a6939321c5bb89e9a04bccc102b1dc13f657b2
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-03-30 21:07:22 -0400

    Cleaned project to remove databinding errors.

--- app/src/main/java/com/example/android/pointmax/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/pointmax/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.pointmax.databinding.FragmentHomeBinding
 import timber.log.Timber
