a3b8ed72236c0d9cb93ce8fb69cfae9d72345d45
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-04-13 19:52:41 -0400

    Update CardDao

--- app/src/main/java/com/example/android/pointmax/database/Card.kt
+++ app/src/main/java/com/example/android/pointmax/database/Card.kt
@@ -1,2 +1,8 @@
 package com.example.android.pointmax.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "card_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "card") val card: String)
--- app/src/main/java/com/example/android/pointmax/database/CardDao.kt
+++ app/src/main/java/com/example/android/pointmax/database/CardDao.kt
@@ -1,2 +1,18 @@
 package com.example.android.pointmax.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM card_table")
+    suspend fun deleteAll()
+}
