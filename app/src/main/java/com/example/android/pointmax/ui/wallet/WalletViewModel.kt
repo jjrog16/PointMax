e7db53ba8c86636cb8d3545bb8355a70dc33eeb0
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-04-21 20:17:10 -0400

    Commented out instances referring to RecyclerView for WalletFragment

--- app/src/main/java/com/example/android/pointmax/ui/wallet/WalletViewModel.kt
+++ app/src/main/java/com/example/android/pointmax/ui/wallet/WalletViewModel.kt
@@ -11,26 +11,26 @@ import com.example.android.pointmax.database.CardRoomDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class WalletViewModel(application: Application) : ViewModel() {
-    private val repository: CardRepository
-    // Using LiveData and caching what getAlphabetizedWords returns has several benefits:
-    // - We can put an observer on the data (instead of polling for changes) and only update the
-    //   the UI when the data actually changes.
-    // - Repository is completely separated from the UI through the ViewModel.
-    val allCards: LiveData<List<Card>>
-
-    init {
-        val cardsDao = CardRoomDatabase.getDatabase(application, viewModelScope).cardDao()
-        repository = CardRepository(cardsDao)
-        allCards = repository.allCards
-    }
-
-    /**
-     * Launching a new coroutine to insert the data in a non-blocking way
-     */
-    fun insert(card: Card) = viewModelScope.launch(Dispatchers.IO) {
-        repository.insert(card)
-    }
+class WalletViewModel/*(application: Application)*/ : ViewModel() {
+//    private val repository: CardRepository
+//    // Using LiveData and caching what getAlphabetizedWords returns has several benefits:
+//    // - We can put an observer on the data (instead of polling for changes) and only update the
+//    //   the UI when the data actually changes.
+//    // - Repository is completely separated from the UI through the ViewModel.
+//    val allCards: LiveData<List<Card>>
+//
+//    init {
+//        val cardsDao = CardRoomDatabase.getDatabase(application, viewModelScope).cardDao()
+//        repository = CardRepository(cardsDao)
+//        allCards = repository.allCards
+//    }
+//
+//    /**
+//     * Launching a new coroutine to insert the data in a non-blocking way
+//     */
+//    fun insert(card: Card) = viewModelScope.launch(Dispatchers.IO) {
+//        repository.insert(card)
+//    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "Wallet"
