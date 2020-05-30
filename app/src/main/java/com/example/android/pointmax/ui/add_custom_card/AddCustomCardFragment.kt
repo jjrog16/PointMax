ccf36e96ba5c623629530c247918164de9b361ec
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-05-30 17:30:06 -0400

    Connected categories to card and fixed issue with being able to add same cards if they are in DB

--- app/src/main/java/com/example/android/pointmax/ui/add_custom_card/AddCustomCardFragment.kt
+++ app/src/main/java/com/example/android/pointmax/ui/add_custom_card/AddCustomCardFragment.kt
@@ -29,14 +29,6 @@ class AddCustomCardFragment : Fragment() {
     
     private lateinit var viewModel: AddCustomCardViewModel
     private lateinit var editCardNameView: EditText
-//    private lateinit var generalEarn: EditText
-//    private lateinit var airlinesEarn: EditText
-//    private lateinit var restaurantsEarn: EditText
-//    private lateinit var groceriesEarn: EditText
-//    private lateinit var gasEarn: EditText
-//    private lateinit var travelEarn: EditText
-    private var isCardInList: Boolean = false // Holds if the card is currently in the database
-    private lateinit var cardBeforeBeingEntered: String
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,11 +40,11 @@ class AddCustomCardFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val application = requireNotNull(activity).application
-    
+        
         // EditText field
         editCardNameView = new_card_name
-    
-    
+        
+        
         // Card value passed in through Fragment as a string
         val cardToChange = AddCustomCardFragmentArgs.fromBundle(requireArguments()).cardToEdit
         
@@ -68,36 +60,17 @@ class AddCustomCardFragment : Fragment() {
         viewModel =
             ViewModelProvider(this, viewModelFactory).get(AddCustomCardViewModel::class.java)
         
-         viewModel.allCards.observe(viewLifecycleOwner, Observer {
-             if (it != null) {
-                 for (card in it){
-                     if (card.cardName == cardBeforeBeingEntered) {
-                         generalEarn.setText(card.general.toString())
-                         airlinesEarn.setText(card.airlines.toString())
-                         restaurantsEarn.setText(card.restaurant.toString())
-                         travelEarn.setText(card.travel.toString())
-                         groceriesEarn.setText(card.groceries.toString())
-                         gasEarn.setText(card.gas.toString())
-                     }
-                 }
+        viewModel.allCards.observe(viewLifecycleOwner, Observer { cardList ->
+            if (cardList != null) {
+                Timber.i("${cardList[0]}")
+                var cardToBeEntered = editCardNameView.text.toString().trim().toUpperCase()
+                setCategoryViews(cardList, cardToBeEntered)
                 
-                 // Add category option so user can add categories to card
-                 add_next_category.setOnClickListener {
-                     Toast.makeText(context, "Add button pressed", Toast.LENGTH_SHORT).show()
-                 }
-             }
-         })
-        
-        // Check if text is empty
-        // If empty, do nothing
-        // If there is text, then start coroutine to load into database
-        add_card_done.setOnClickListener {
-            // Take new entered input
-            val cardToBeEntered = editCardNameView.text.toString().trim()
-            
-            viewModel.allCards.observe(viewLifecycleOwner, Observer {
-                if (it != null) {
-                    isCardInList = it.contains(Card(cardToBeEntered.toUpperCase()))
+                add_card_done.setOnClickListener {
+                    cardToBeEntered = editCardNameView.text.toString().trim().toUpperCase()
+                    //isCardInList = cardList.contains(Card(cardToBeEntered))
+                    Timber.i("[Done Clicked] Card to enter: $cardToBeEntered")
+                    Timber.i("[Done Clicked] Is Petal card in list: ${cardList.contains(Card("PETAL CREDIT CARD"))}")
                     when {
                         // Case when nothing is in EditText
                         TextUtils.isEmpty(editCardNameView.text) or
@@ -111,17 +84,20 @@ class AddCustomCardFragment : Fragment() {
                         // Sets the text of the EditText view only if coming from another card
                         isComingFromAnotherCard -> {
                             // Edit the card only if the card entered is not the same
-                            if (!isCardInList) {
+                            if (!setCategoryViews(cardList,cardToBeEntered)) {
                                 cardToBeEntered.let {
-                                    viewModel.edit(oldName = cardToChange, newName = cardToBeEntered.toUpperCase())
-                                    
+                                    viewModel.edit(
+                                        oldName = cardToChange,
+                                        newName = cardToBeEntered.toUpperCase()
+                                    )
                                 }
                                 // Go back to wallet after finishing the edit
-                                val action = AddCustomCardFragmentDirections.actionAddCustomCardFragmentToNavigationWallet()
+                                val action =
+                                    AddCustomCardFragmentDirections.actionAddCustomCardFragmentToNavigationWallet()
                                 findNavController().navigate(action)
-                
+            
                                 // Hides keyboard after finishing input
-                                context?.let { it1 -> hideKeyboard(it1,editCardNameView) }
+                                context?.let { it1 -> hideKeyboard(it1, editCardNameView) }
                             } else {
                                 Toast.makeText(
                                     context,
@@ -132,11 +108,16 @@ class AddCustomCardFragment : Fragment() {
                         }
                         else -> {
                             // Adding a new card into the Wallet
-                            if(!isCardInList) {
+                            if (!setCategoryViews(cardList,cardToBeEntered)) {
                                 // Insert a new card
-                                cardToBeEntered.let {cardNameToEnter ->
+                                cardToBeEntered.let { cardNameToEnter ->
                                     val card = Card(cardNameToEnter.toUpperCase())
                                     viewModel.insert(card)
+    
+                                    // Once value is added into the database, go back to the wallet
+                                    val action =
+                                        AddCustomCardFragmentDirections.actionAddCustomCardFragmentToNavigationWallet()
+                                    findNavController().navigate(action)
                                 }
                             } else {
                                 Toast.makeText(
@@ -145,26 +126,27 @@ class AddCustomCardFragment : Fragment() {
                                     Toast.LENGTH_SHORT
                                 ).show()
                             }
-            
-                            // Once value is added into the database, go back to the wallet
-                            val action =
-                                AddCustomCardFragmentDirections.actionAddCustomCardFragmentToNavigationWallet()
-                            findNavController().navigate(action)
-            
+        
                             // Hides keyboard after finishing input
-                            context?.let { it1 -> hideKeyboard(it1,editCardNameView) }
+                            context?.let { it1 -> hideKeyboard(it1, editCardNameView) }
                         }
                     }
+                    
                 }
-            })
-        }
-    }
     
+                // Add category option so user can add categories to card
+                add_next_category.setOnClickListener {
+                    Toast.makeText(context, "Add button pressed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
     // Called after the operation is completed in order to hide the keyboard when EditText field
     // is gone.
     private fun hideKeyboard(context: Context, editText: EditText) {
-        val imm: InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(editText.windowToken,0)
+        val imm: InputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(editText.windowToken, 0)
     }
     
     // If the user selects to add a new card, the default value passed is an empty string,
@@ -176,9 +158,27 @@ class AddCustomCardFragment : Fragment() {
         if (!cardToChange.matches("\\s*".toRegex())) {
             isValuePassed = true
             editCardNameView.setText(cardToChange)
-            cardBeforeBeingEntered = editCardNameView.text.toString().trim().toUpperCase()
         }
         return isValuePassed
     }
+    
+    private fun setCategoryViews(cardList: List<Card>, cardToBeEntered: String):Boolean {
+        var cardContained = false
+        for (card in cardList) {
+            if (card.cardName == cardToBeEntered) {
+                generalEarn.setText(card.general.toString())
+                airlinesEarn.setText(card.airlines.toString())
+                restaurantsEarn.setText(card.restaurant.toString())
+                travelEarn.setText(card.travel.toString())
+                groceriesEarn.setText(card.groceries.toString())
+                gasEarn.setText(card.gas.toString())
+                cardContained = true
+            }
+        }
+        return cardContained
+    }
 }
+
+
+
 
