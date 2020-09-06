27082319e94d7cd0f95676dfd6e59f5c66c051c2
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-09-05 21:52:42 -0400

    Changed value in RecommendedViewModel.kt to "Coming Soon..."

--- app/src/main/java/com/example/android/pointmax/ui/recommended/RecommendedViewModel.kt
+++ app/src/main/java/com/example/android/pointmax/ui/recommended/RecommendedViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class RecommendedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Recommended"
+        value = "Coming Soon..."
     }
     val text: LiveData<String> = _text
 }
