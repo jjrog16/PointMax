bd752e594b1625d004ff0953d1ed5198f7b332de
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-09-07 23:08:45 -0400

    Initialized DAO and Room database before each instrumented test.

--- app/src/androidTest/java/com/example/android/pointmax/database/CardDaoTest.kt
+++ app/src/androidTest/java/com/example/android/pointmax/database/CardDaoTest.kt
@@ -1,6 +1,9 @@
 package com.example.android.pointmax.database
 
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -8,5 +11,20 @@ import org.junit.runners.JUnit4
 class CardDaoTest {
 
     private lateinit var database: CardRoomDatabase
+    private lateinit var dao: CardDao
+    
+    /**
+     * Before each test, initialize a new Room database, store it in RAM memory, and initialize
+     * the DAO
+     */
+    @Before
+    fun setup(){
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            CardRoomDatabase::class.java
+        ).allowMainThreadQueries().build()
+        
+        dao = database.cardDao()
+    }
 
 }
